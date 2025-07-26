a3bf7c1e49e7404f3c7e57fbd20c88660f436d1e
faheem221 <faheemk793@gmail.com>
2025-07-26 13:19:33 +0530

    added link to logo

--- app/app_chunks/Logo.tsx
+++ app/app_chunks/Logo.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 const Logo = () => {
   return (
-    <div>
-      <Image src={"/Logo.png"} width={170} height={20} alt={'Logo'} />
-    </div>
+    <Link href={"/"}>
+      <Image src={"/Logo.png"} width={170} height={20} alt={"Logo"} />
+    </Link>
   );
 };
 
