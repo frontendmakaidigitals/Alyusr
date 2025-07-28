2e3e57b6e12b5535e82933461efbc25b38094c5f
faheem221 <faheemk793@gmail.com>
2025-07-28 13:27:56 +0530

    fix

--- app/app_chunks/Logo.tsx
+++ app/app_chunks/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href={"/"}>
-      <Image src={"/Logo.png"} width={170} height={20} alt={"Logo"} />
+      <Image src={"/Logo2.png"} width={170} height={20} alt={"Logo"} />
     </Link>
   );
 };
