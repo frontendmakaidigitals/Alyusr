94001d01123d871a68101d3f01310f67eea53b6a
faheem221 <faheemk793@gmail.com>
2025-09-19 10:44:15 +0530

    added

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import fs from "fs";
 import path from "path";
 
@@ -59,7 +59,7 @@ export async function PUT(req: Request) {
 }
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
