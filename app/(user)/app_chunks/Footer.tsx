38f78d9ab2baa64abd5a7dd22c824016437c9e62
faheem221 <faheemk793@gmail.com>
2025-10-27 16:23:28 +0530

    fixed footer

--- app/(user)/app_chunks/Footer.tsx
+++ app/(user)/app_chunks/Footer.tsx
@@ -174,7 +174,7 @@ const Footer = () => {
       {/* Bottom bar */}
       <div className="bg-dimondra-tealDark text-slate-50 text-sm text-center py-3 relative z-10">
         &copy; {new Date().getFullYear()} All Rights Reserved – Powered by{" "}
-        <span className="text-blue-500">Alyusr Engineering</span>
+        <span className="text-blue-500">Insight Vision Marketing</span>
       </div>
     </footer>
   );
