66ff2b6b3214a40931554f0e038e2d1194b3ef2e
frontendmakaidigitals <frontendmakaidigitals@gmail.com>
2025-12-31 14:16:21 +0530

    address changed

--- app/(user)/app_chunks/Footer.tsx
+++ app/(user)/app_chunks/Footer.tsx
@@ -150,8 +150,7 @@ const Footer = () => {
                   <MapPin className="w-5 h-5 mt-1" />
                   <p className="max-w-[250px]">
                     {" "}
-                    Al Rawdah District, King Abdullah Road Riyadh 13313 – 8119,
-                    Saudi Arabia
+                   Al-Aqeeq District, Prince Mohammad bin Sa’ad bin Abdulaziz Road, Riyadh 13511, Saudi Arabia
                   </p>
                 </div>
                 <div className="flex items-center gap-2">
